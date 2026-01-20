e020d29e55bb76e62aea3deb6610921eaf3abd2f
Shipra Upadhyay <117964967+Shipra53@users.noreply.github.com>
2026-01-20 11:32:29 +0530

    Print n star vertical

---
+++ printnstarver.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class printnstarver {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println("*");
+        }
+        
+    }
+
+}
