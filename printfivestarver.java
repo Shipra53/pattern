a89fbf3f250c4e6df008246acef402649132f7bb
Shipra Upadhyay <117964967+Shipra53@users.noreply.github.com>
2026-01-20 11:35:38 +0530

    Print 5 star vertical

---
+++ printfivestarver.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class printfivestarver {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n=5;
+        for(int i=0;i<n;i++){
+            System.out.println("*");
+        }
+        
+    }
+
+    
+}
