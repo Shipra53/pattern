b99c10944e741f25d31998bfa39953765e577111
Shipra Upadhyay <117964967+Shipra53@users.noreply.github.com>
2026-01-22 23:56:31 +0530

    Print a line of alternating stars and spaces

---
+++ PrintAltStars.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+//Print a line of alternating stars and spaces
+
+public class PrintAltStars {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            if(i%2!=0){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+        sc.close();
+    }
+}
