7ac473f94ae25cfd7f962160818df1bb51d681ce
Shipra Upadhyay <117964967+Shipra53@users.noreply.github.com>
2026-01-22 22:56:06 +0530

    Print 5 star in a row

---
+++ printfivestarhor.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+//Print 5 start in one row
+public class printfivestarhor {
+     public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n=5;
+        for(int i=0;i<n;i++){
+            System.out.print("*");
+        }
+        
+    }
+}
