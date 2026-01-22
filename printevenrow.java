21e1ddfff5ecfc23555a5466c2595a615720c901
Shipra Upadhyay <117964967+Shipra53@users.noreply.github.com>
2026-01-23 00:00:02 +0530

    Print stars (*) only on even-numbered rows

---
+++ printevenrow.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+//Print stars (*) only on even-numbered rows
+public class printevenrow {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int r,c;
+        r=sc.nextInt();
+       for (int i = 1; i <= r; i++) {
+            if (i % 2 == 0) {
+                for (int j = 1; j <= r-1; j++) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
