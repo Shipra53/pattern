fee206238660a3120dbcc4928dcce0a8a0bf2d4d
Shipra Upadhyay <117964967+Shipra53@users.noreply.github.com>
2026-01-28 11:48:17 +0530

    Print Inverted Full Diagonal

---
+++ PrintInvertFullDiag.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PrintInvertFullDiag {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i-1;k++){
+                System.out.print("*");
+                sc.close();
+            }
+            System.out.println();
+        }
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
